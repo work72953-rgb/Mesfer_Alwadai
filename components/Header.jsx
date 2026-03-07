9dd3ec08c642eb64735264d56033aadd9916ad3c
Mahmoud Elbalhi <mahmoud01234elbalhi@gmail.com>
2026-03-07 15:59:40 +0200

    up

--- components/Header.jsx
+++ components/Header.jsx
@@ -14,10 +14,12 @@ export default function Header() {
     <div
       className={`h-20 w-full bg-[#000000d7] text-white flex justify-between items-center p-5`}
     >
-      <span className={`text-xl md:text-3xl font-bold`}>مسفر الوادعي</span>
+      <span className={`text-md md:text-3xl font-bold`}>
+        {lang==='ar'?"مسفر الوادعي":"Mesfer Alwadai"}
+      </span>
       {/* <Image width={150} height={150} src={'/logo.png'} alt="logo"/> */}
       <nav className={`p-1 flex justify-center items-center gap-5`}>
-        <a href="#footer_">تواصل معي</a>
+        <a href="#footer_">{lang==="ar"?"تواصل معي":"Contact Me"}</a>
         <select
           value={lang}
           onChange={handleChange}
